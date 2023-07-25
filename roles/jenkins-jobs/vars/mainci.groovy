d5c3ddb8ff6d988cf77d17255d57376e4e42bf71
Shravang007 <shravangoudgajula@gmail.com>
2023-07-25 21:25:25 +0530

    Jenkinsfile

--- roles/jenkins-jobs/vars/mainci.groovy
+++ roles/jenkins-jobs/vars/mainci.groovy
@@ -1,42 +1,29 @@
 def call() {
-    pipeline {
+    node('workstation') {
 
-        agent {
-            node { label 'workstation'}
-        }
-
-        stages {
-
+        if (env.cibuild == "java") {
             stage('Build') {
                 steps {
                     sh 'mvn package'
-                }
-            }
-
+        }
+    }
             stage('Unit tests') {
-                steps {
                     echo 'unit tests'
                     // sh 'mvn test'
-                }
             }
 
             stage('Code Analysis') {
-                steps {
                     echo 'sonar'
-                    // sh 'sonar-scanner -Dsonar.host.url=http://172.31.93.52:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend'
-                }
+                    // sh 'sonar-scanner -Dsonar.host.url=http://172.31.93.52:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend
             }
 
             stage('Security Scans') {
-                steps {
                     echo 'Security Scans'
-                }
             }
 
             stage('Publish a Artifact') {
-                steps {
+
                     echo 'Publish a Artifact'
-                }
             }
 
 
